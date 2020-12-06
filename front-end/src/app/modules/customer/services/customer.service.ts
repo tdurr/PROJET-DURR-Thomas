e29c48dc6d2fba07c929a4192bccc108c3618501
Thomas DURR <thms.durr@gmail.com>
2020-12-06 04:25:16 +0100

    on observe

--- front-end/src/app/modules/customer/services/customer.service.ts
+++ front-end/src/app/modules/customer/services/customer.service.ts
@@ -15,19 +15,18 @@ export class CustomerService {
     return this.http.get<Client>(environment.apiUrl + 'customers/' + login);
   }
 
-  login(login: string, password: string): Promise<any> {
+  login(login: string, password: string): Observable<{success: boolean}> {
     let body = new URLSearchParams();
     body.set('login', login);
     body.set('password', password);
 
-    return this.http.post<any>(
+    return this.http.post<{success: boolean}>(
       environment.apiUrl + 'user/login',
       body.toString(),
       {
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        observe: 'response'
       }
-      ).toPromise();
+    );
   }
 
   register(client: Client): Observable<{success: boolean, login: string}> {
