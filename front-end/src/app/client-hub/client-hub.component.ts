c1181067ac32b94f31841c9445436f193f83d08d
Thomas DURR <thms.durr@gmail.com>
2020-12-06 02:41:55 +0100

    updates UX

--- front-end/src/app/client-hub/client-hub.component.ts
+++ front-end/src/app/client-hub/client-hub.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ClientState } from '../store/states/client-state';
+import { Store } from '@ngxs/store';
+import { AddJWT, AddLogin } from './../../app/store/actions/client-action';
 
 @Component({
   selector: 'app-client-hub',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientHubComponent implements OnInit {
 
-  constructor() { }
+  public login: Observable<string>;
+  public userConnected: boolean = true;
+
+  constructor(private store : Store) { }
 
   ngOnInit(): void {
+    this.login = this.store.select(ClientState.getLogin);
+
+    if (this.store.select(ClientState.getTokenJwt)) {
+      this.userConnected = false;
+    }
+  }
+
+  onDisconnect() {
+    this.store.dispatch(new AddJWT(''));
+    this.store.dispatch(new AddLogin(''));
+    this.userConnected = false;
   }
 
 }
